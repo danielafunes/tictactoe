f773beda199ab2b59fc38b7baaf764093af6b28f
danielafunes <00158916@uca.edu.sv>
2019-11-01 11:37:16 -0600

    Updates

---
+++ precache-manifest.a753b4752a210e44d4e8c7accd72a970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b05fa632f40628dbfe5bcf983d4e38",
+    "url": "/danielafunes/tictactoe.git/index.html"
+  },
+  {
+    "revision": "c0305dbb8f6a0000a60e",
+    "url": "/danielafunes/tictactoe.git/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "/danielafunes/tictactoe.git/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "c0305dbb8f6a0000a60e",
+    "url": "/danielafunes/tictactoe.git/static/js/main.19c8a519.chunk.js"
+  },
+  {
+    "revision": "5a37c2ff691d058e4cff",
+    "url": "/danielafunes/tictactoe.git/static/js/runtime-main.b074559e.js"
+  }
+]);
